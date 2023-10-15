77770e61d04d0fc92ef305445e235d99f6a458da
Alexandra Ngaleba-Ze <89806302+ProcessOptimizingDataAnalyst@users.noreply.github.com>
2023-10-15 16:12:21 -0400

    Update Guess the Number (User).py

--- Guess the Number (User).py
+++ Guess the Number (User).py
@@ -1,7 +1,7 @@
 import random
 
 def guess(x):
-    random_number = random.randint(1, x) # The randint() method returns an integer number selected element from the specified range.
+    random_number = random.randint(1, x) 
     guess = 0
     while guess != random_number:
         guess = int(input(f'guess a number between 1 and {x} '))
